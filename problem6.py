8d3108f93a33acbdc1b85f926aa5968d6e6c8088
Szymon Wieczorek <fivtyen@gmail.com>
2016-10-23 20:01:49 +0200

    Update problem6.py

--- problem6.py
+++ problem6.py
@@ -1,8 +1,7 @@
 # Project Euler no. 6
 # Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 
-result1 = 0
-result2 = 0
+result1, result2 = 0, 0
 
 for i in range (1, 101):
     result1 += i*i
