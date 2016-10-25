895696ac9338f355feca1f28815b3884211d9223
Szymon Wieczorek <fivtyen@gmail.com>
2016-10-25 12:14:29 +0200

    Create problem10.py

---
+++ problem10.py
@@ -0,0 +1,18 @@
+# Project Euler no. 10
+# Find the sum of all the primes below two million.
+
+import math
+
+result = 0
+
+
+def is_prime(n):
+    if n % 2 == 0 and n > 2:
+        return False
+    return all(n % i for i in range(3, int(math.sqrt(n)) + 1, 2))
+
+for i in range(2, 2000000):
+    if is_prime(i):
+        result += i
+
+print(result)
