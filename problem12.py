20359f68dd6d0fe2d339f874abe9c756d9835025
Szymon Wieczorek <fivtyen@gmail.com>
2016-11-07 21:27:30 +0100

    Create problem12.py

---
+++ problem12.py
@@ -0,0 +1,23 @@
+# Project Euler problem no. 12
+# What is the value of the first triangle number to have over five hundred divisors?
+
+from math import sqrt
+
+def factors(number):
+    counter = 0
+    s_root = int(sqrt(number))
+    for i in range(1, s_root):
+        if number%i == 0:
+            counter += 2
+    if (s_root*s_root == number):
+        counter -= 1
+    return counter
+
+
+number = 1
+to_add = 2
+
+while(factors(number) < 500):
+    number += to_add
+    to_add += 1
+print(number)
