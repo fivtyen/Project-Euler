8b1f221eaae9333c073abf640ab6a446728744cc
Szymon Wieczorek <fivtyen@gmail.com>
2016-11-10 20:20:12 +0100

    Create problem22.py

---
+++ problem22.py
@@ -0,0 +1,17 @@
+# Project Euler problem no. 22
+
+tab =[]
+
+with open("p022_names.txt") as name_file:
+    for word in name_file.read().split(","):
+        tab.append(str(word))
+
+value = 0
+tab = sorted(tab)
+
+for i in range(len(tab)):
+    word = tab[i]
+    for j in range(1, len(word)-1):
+        value += (ord(word[j]) - 64) * (i + 1)
+
+print(value)
