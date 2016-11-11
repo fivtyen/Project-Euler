12503004d764287db22621bde54b068a6a0fed3e
Szymon Wieczorek <fivtyen@gmail.com>
2016-11-11 14:42:19 +0100

    Create problem23.py

---
+++ problem23.py
@@ -0,0 +1,33 @@
+#Project Euler problem no. 23
+
+limit = 28123
+
+def check_abundant(number):
+    value = 0
+    for i in range(1, number):
+        if number%i == 0:
+            value += i
+    if value > number:
+        return True
+
+ab_numbers = []
+result = 0
+
+for i in range(2, limit):
+    if check_abundant(i) == True:
+        ab_numbers.append(i)
+
+check_list = [None] * (limit + 1)
+
+for i in range(len(ab_numbers)):
+    for j in range(i, limit):
+        if (ab_numbers[i] + ab_numbers[j]) <= limit:
+            check_list[ab_numbers[i] + ab_numbers[j]] = 1
+        else:
+            break
+
+for i in range(len(check_list)):
+    if check_list[i] != 1:
+        result += i
+
+print(result)
