884da0358bc37df8df76925774fc6507a86ec7e2
Szymon Wieczorek <fivtyen@gmail.com>
2017-03-14 00:01:59 +0100

    Update problem24.py

--- problem24.py
+++ problem24.py
@@ -3,4 +3,4 @@
 import itertools
 
 perm_list = sorted(list(itertools.permutations(list(range(10)))))
-print(perm_list[999999])
+print(''.join(str(perm_list[999999]).replace(',','').replace(' ', '')))
