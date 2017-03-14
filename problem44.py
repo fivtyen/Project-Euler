548b26d1b37af6c8b1d1f6b522c933ab7ca24b70
Szymon Wieczorek <fivtyen@gmail.com>
2017-03-14 12:01:43 +0100

    Create problem44.py

---
+++ problem44.py
@@ -0,0 +1,24 @@
+import math
+
+
+def is_pentagonal(x):
+    n = (math.sqrt(24*x+1) + 1)/6
+    return n == int(n)
+
+
+def pentagonal(n):
+    return int((n*(3*n-1)/2))
+
+go = True
+i = 1
+
+while go:
+    i += 1
+    n = pentagonal(i)
+    for j in range(1, i):
+        m = pentagonal(j)
+        if is_pentagonal(n+m) == True and is_pentagonal(abs(n-m)) == True:
+            result = abs(pentagonal(i) - pentagonal(j))
+            print(result)
+            go = False
+            break
