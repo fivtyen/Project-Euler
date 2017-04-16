e34678c041d9241d243b8df0e8ea4d6f3c65c0c9
Szymon Wieczorek <fivtyen@gmail.com>
2017-04-16 15:59:00 +0200

    p43

---
+++ problem43.py
@@ -0,0 +1,21 @@
+import itertools
+
+Pandigitals = list(itertools.permutations(range(10), 10))
+sPandigitals = []
+divisors = [0, 2, 3, 5, 7, 11, 13, 17]
+result = 0
+
+for i in range(len(Pandigitals)):
+    n = (str(Pandigitals[i]).replace("(", "").replace(")", "").replace(",", "").replace(" ", ""))
+    sPandigitals.append(n)
+
+for j in sPandigitals:
+    c = 0
+    for k in range(1, len(j)-2):
+        a = (int(j[k] + j[k+1] + j[k+2]))
+        if a % divisors[k] == 0:
+            c += 1
+    if c == 7:
+        result += int(j)
+
+print(result)
